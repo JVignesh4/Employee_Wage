6019a878957552dd18b6c6e9905fc8a9305bd9ad
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-20 10:08:00 +0530

    Added: Employee Attendance Details

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,13 @@
 
 public class Employee {
+    static int present = 1;
+
+    static void attendanceOfEmployee() {
+        double empCheck = Math.floor((Math.random() * 10) % 2);
+        if (empCheck == present) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
 }
