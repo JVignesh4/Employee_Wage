422e158b9a6978e7712715d5b8caad2b7e745170
JVignesh4 <vigneshbuji04@gmail.com>
2022-06-20 11:27:45 +0530

    Added: Daily Wage Calculation Of Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,9 @@
 
 public class Employee {
+
     static int present = 1;
+    static int Wage_Per_Hour = 20;
+    static int Total_Working_Hrs = 8;
 
     static void attendanceOfEmployee() {
         double empCheck = Math.floor((Math.random() * 10) % 2);
@@ -10,4 +13,12 @@ public class Employee {
             System.out.println("Employee is Absent");
         }
     }
+
+    static void wageOfEmployee() {
+        double empCheck = Math.floor((Math.random() * 10) % 1 + 1);
+        if (present == empCheck) {
+            System.out.println("Employee wage per day = " + Total_Working_Hrs * Wage_Per_Hour);
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Employee.attendanceOfEmployee();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("1.Employee Attendance");
+        System.out.println("2.Wage per day for the Employee");
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                Employee.attendanceOfEmployee();
+                break;
+            case 2:
+                Employee.wageOfEmployee();
+                break;
+            default:
+                System.out.println("Choose the Correct Option");
+        }
     }
 }
